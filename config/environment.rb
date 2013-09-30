c239a1387f5adac9ca8f72149bbf32a8c4822c23
William Karsten <yme_ynot45@hotmail.com>
2013-09-30 15:29:58 -0500

    Setup

---
+++ config/environment.rb
@@ -0,0 +1,32 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+require 'rubygems'
+
+require 'uri'
+require 'pathname'
+
+require 'pg'
+require 'active_record'
+require 'logger'
+
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+require 'erb'
+require 'bcrypt'
+require 'debugger'
+require 'pry'
+
+# Some helper constants for path-centric logic
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+# Set up the controllers and helpers
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+
+# Set up the database and models
+require APP_ROOT.join('config', 'database')
